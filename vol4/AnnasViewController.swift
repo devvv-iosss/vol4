f54d417df46ad0a4f266047fc6f9bca9543ca4ee
devvv-iosss <81225988+devvv-iosss@users.noreply.github.com>
2021-10-11 10:05:35 +0300

    added new storyboard and VC

---
+++ vol4/AnnasViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AnnasViewController.swift
+//  vol4
+//
+//  Created by Anna on 11.10.2021.
+//
+
+import UIKit
+
+class AnnasViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
